45ab5d01b80400b5ec0cb63f3d9bb0c79bd3bc38
Aman <amanbhimla70@gmail.com>
2025-05-12 13:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+const romanMap ={
 
-  //your code here
+0: ['M',1000],
+1: ['CM',900],
+2: ['D',500],
+3: ['CD',400],
+4: ['c',100],
+5: ['xc',90],
+6: ['l',50],
+7: ['xl',40],
+8: ['x',10],
+9: ['lx',9],
+10: ['v',5],
+11: ['IV',4],
+12: ['I',1]
 
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+};
+let n=798;
+let ans=""
+while(n!=0)
+  {
+    for(let i in romanMap)
+       {
+         let value=romanMap[i][0];
+         let num=romanMap[i][1]
+         if(num<=n)
+           {
+             ans=ans+value;
+             n=n-num;
+             break;
+           }
+       }
+  }
